b01a0dfdc4d41eeed318c1a5ac5f21d49791af33
Livhuwani Maphupha <maphuphaprince737@gmail.com>
2024-06-19 21:39:31 +0000

    First Commit

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,21 @@
 import socket
+import nmap
+
+scanner = nmap.PortScanner()
 
 def get_open_ports(target, port_range, verbose = False):
     open_ports = []
 
+    if(verbose):
+        scanner.scan(target, '1-1024', '-v sS -sV -sC -A -O')
+        open_ports = scanner[target]['tcp'].keys()
+    else:
+        scanner.scan(target, '1-1024', 'sS -sV -sC -A -O')
+        open_ports = scanner[target]['tcp'].keys()
+    
+
+    print(open_ports)
+    
 
 
     return(open_ports)
