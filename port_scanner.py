cef5061a9afd43b90b05a92e337f59080e8e6ce5
Livhuwani Maphupha <maphuphaprince737@gmail.com>
2024-06-19 22:13:36 +0000

    port scanner

--- port_scanner.py
+++ port_scanner.py
@@ -1,21 +1,44 @@
 import socket
-import nmap
+from socket import AF_INET, SOCK_STREAM
+from common_ports import ports_and_services
 
-scanner = nmap.PortScanner()
 
-def get_open_ports(target, port_range, verbose = False):
+def get_open_ports(target, port_range, verbose_mode=False):
     open_ports = []
 
-    if(verbose):
-        scanner.scan(target, '1-1024', '-v sS -sV -sC -A -O')
-        open_ports = scanner[target]['tcp'].keys()
+    for port in range(port_range[0], port_range[1] + 1):
+        server_socket = socket.socket(family=AF_INET, type=SOCK_STREAM)
+        server_socket.settimeout(1)
+        socket.setdefaulttimeout(1)
+        try:
+            result = server_socket.connect_ex((target, port))
+            if result == 0:
+                open_ports.append(port)
+        except socket.gaierror:
+            if target[0].isalpha():
+                return "Error: Invalid hostname"
+            return "Error: Invalid IP address"
+
+        server_socket.close()
+
+    if not verbose_mode:
+        return open_ports
+
+    if not target[0].isalpha():
+        ip_addr = target
+        try:
+            target = socket.gethostbyaddr(ip_addr)[0]
+        except socket.herror:
+            target = ""
     else:
-        scanner.scan(target, '1-1024', 'sS -sV -sC -A -O')
-        open_ports = scanner[target]['tcp'].keys()
-    
+        ip_addr = socket.gethostbyname(target)
 
-    print(open_ports)
-    
+    if target:
+        ans = f"Open ports for {target} ({ip_addr})\nPORT     SERVICE\n"
 
+    else:
+        ans = f"Open ports for {ip_addr}\nPORT     SERVICE\n"
 
-    return(open_ports)
+    for port in open_ports:
+        ans += f"{port:<9}{ports_and_services[port]}\n"
+    return ans.strip()
